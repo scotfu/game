77dce5cff4c6493f2c42fd3b4f0d24fefa556dbd
fu <scotfu@gmail.com>
2014-06-08 14:39:49 -0400

    add waiting

--- records/urls.py
+++ records/urls.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python
 #coding=utf-8
 from django.conf.urls import patterns, url
-from .views import push,pull
+from .views import push,pull,player_status
 
 
 urlpatterns = patterns('',
     url(r'^record/push/$', push, name='push'),
     url(r'^record/pull/$', pull, name='pull'),
+    url(r'^player_status/$', player_status, name='status'),                       
     )
--- records/views.py
+++ records/views.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse,Http404
@@ -10,6 +11,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.db.models import Q
 
 from .models import Record,Group
+
+NUM_OF_PLAYERS = 2
 
 @csrf_exempt
 def json_response(func):
@@ -54,13 +57,14 @@ def push(request):
 @json_response
 def pull(request):
     if request.method =='GET':
+        timeout = 5
         group_id = request.session.get('group_id',None)
         player = request.GET.get('player',None)
         g_round = request.GET.get('g_round',None)
         
         data = {}
         data['records']=[]
-        while not data['records']:
+        while not data['records'] and timeout:
             try:
                 group = Group.objects.get(id=group_id)
                 records = Record.objects.filter(~Q(player=player),group=group, g_round=1)
@@ -70,10 +74,31 @@ def pull(request):
                     data['records'].append(
                                   {'player':record.player,
                                    'result':record.result,})
+            time.sleep(1)
+            timeout = timeout - 1
         return data
     else:
         return {'result':'false','msg':'Please use GET'}
 
+@json_response
+def player_status(request):
+    if request.method =='GET':
+        group_id = request.session.get('group_id',None)
+        g_round = request.GET.get('g_round',None)
+        data = {}
+        try:
+            group = Group.objects.get(id=group_id)
+        except Exception as e:
+            data = {'result':'false','msg':e}
+        if group.num_of_players == NUM_OF_PLAYERS:
+            data = {'result':'true','msg':"Ready to play"}
+        else:
+            data = {'result':'false',
+                    'msg':"Waiting for others,need "+ str(NUM_OF_PLAYERS -group.num_of_players) +" more players"                   }
+        return data
+    else:
+        return {'result':'false','msg':'Please use GET'}
+                
 
 def render_game(request):
     request.session.set_expiry(0)
@@ -85,7 +110,7 @@ def render_game(request):
             try:
                 group = Group.objects.all().order_by('id').reverse()[0]
                 print group,group.num_of_players
-                if group.num_of_players >= 2:
+                if group.num_of_players >= NUM_OF_PLAYERS:
                     raise Exception,'The group is full'
             except Exception as e:
                 print e,'I just created a new group'
@@ -103,3 +128,4 @@ def render_game(request):
                                   context_instance=RequestContext(request))
     else:
         return Http404
+
