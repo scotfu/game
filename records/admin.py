c21294c851c38e8a6abe4850a7e07967f4823f09
fu <scotfu@gmail.com>
2014-06-10 13:10:29 -0400

    update

--- records/admin.py
+++ records/admin.py
@@ -5,10 +5,10 @@ from .models import Record,Group
 
 
 class RecordAdmin(admin.ModelAdmin):
-    list_display = ('id','player','result')
+    list_display = ('id','player','result','create_date')
 
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ('id','name','num_of_players')
+    list_display = ('id','name','num_of_players','create_date')
 
 
     
